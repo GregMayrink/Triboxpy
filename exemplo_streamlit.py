efef2b57c9a7301ed2b0788a654db303ef001f9c
GregMayrink <139715483+GregMayrink@users.noreply.github.com>
2023-07-18 23:16:07 -0300

    Add files via upload

---
+++ exemplo_streamlit.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+def main():
+    # Título da página
+    st.title("Exemplo de Texto no Streamlit")
+
+    # Texto na página
+    st.write("Olá! Este é um exemplo de texto exibido no Streamlit.")
+
+if __name__ == '__main__':
+    main()
