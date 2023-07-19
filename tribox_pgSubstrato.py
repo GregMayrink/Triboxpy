69f888f4428f5737ad975b0982cca830b46e1b3c
GregMayrink <139715483+GregMayrink@users.noreply.github.com>
2023-07-19 00:36:50 -0300

    Update tribox_pgSubstrato.py

--- tribox_pgSubstrato.py
+++ tribox_pgSubstrato.py
@@ -4,10 +4,9 @@ import altair as alt
 from PIL import Image
 
 # Load data function
-@st.cache_data
-file = st.file_uploader("tribox_dados_substrato.xlsx", type=["xls", "xlsx"])
+@st.cache
 def import_xls_file():
-    
+    file = st.file_uploader("tribox_dados_substrato.xlsx", type=["xls", "xlsx"])
     if file is not None:
         try:
             df = pd.read_excel(file)
@@ -15,6 +14,9 @@ def import_xls_file():
         except Exception as e:
             st.error(f"Erro ao ler o arquivo: {e}")
     return None
+# Load data8
+df = import_xls_file()
+
 
 # Sidebar
 st.sidebar.title('Monitoramento TriBox')
@@ -23,35 +25,38 @@ st.sidebar.image(logo, use_column_width=True)
 st.sidebar.subheader('Filtros')
 
 
-if df is not None:
+with st.sidebar:
+    st.title('Monitoramento TriBox')
+    logo = Image.open('favicon (1).ico') 
+    st.image(logo, use_column_width=True)
+    st.subheader('Filtros')
+
     # Filtering options
-    fbox = st.sidebar.multiselect('BOX', options=df['box'].unique())
-    fcliente = st.sidebar.selectbox('CLIENTE', options=df['cliente'].unique())
-    flote = st.sidebar.selectbox('LOTE', options=df['lote'].unique())
-    ftratamentos = st.sidebar.multiselect('TRATAMENTOS', options=df['tratamentos'].unique())
-    fvar = st.sidebar.multiselect('VARIAVEIS', options=df['var'].unique())
+    fbox = st.multiselect('BOX', options=df['box'].unique())
+    fcliente = st.selectbox('CLIENTE', options=df['cliente'].unique())
+    flote = st.selectbox('LOTE', options=df['lote'].unique())
+    ftratamentos = st.multiselect('TRATAMENTOS', options=df['tratamentos'].unique())
+    fvar = st.multiselect('VARIAVEIS', options=df['var'].unique())
 
-    # Apply filters
-    filtered_data = df.loc[
-        (df['box'].isin(fbox)) &
-        (df['cliente'] == fcliente) &
-        (df['lote'] == flote) &
-        (df['tratamentos'].isin(ftratamentos)) &
-        (df['var'].isin(fvar))
-    ]
+# Apply filters
+filtered_data = df.loc[
+    (df['box'].isin(fbox)) &
+    (df['cliente'] == fcliente) &
+    (df['lote'] == flote) &
+    (df['tratamentos'].isin(ftratamentos)) &
+    (df['var'].isin(fvar))
+]
 
-    # Line chart
-    if not filtered_data.empty:
-        chart = alt.Chart(filtered_data).mark_line().encode(
-            x='data:T',
-            y='valor:Q',
-            color='tratamentos:N',
-            strokeWidth=alt.value(3)
-        )
-        st.header('Linha do tempo')
-        st.markdown(f"**Variáveis selecionadas:** {', '.join(fvar)}")
-        st.altair_chart(chart)
-    else:
-        st.warning('Nenhum dado encontrado! Selecione os filtros.')
+# Line chart
+if not filtered_data.empty:
+    chart = alt.Chart(filtered_data).mark_line().encode(
+        x='data:T',
+        y='valor:Q',
+        color='tratamentos:N',
+        strokeWidth=alt.value(3)
+    )
+    st.header('Linha do tempo')
+    st.markdown(fvar)
+    st.altair_chart(chart)
 else:
-    st.warning('Nenhum arquivo carregado! Faça o upload de um arquivo Excel.')
+    st.warning('Nenhum dado encontrado! Selecione os filtros.')
